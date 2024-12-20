596d76717404f8cfed169d31c9206999d4272c98
Pavan Kumar <pavankumarts2001@gmail.com>
2024-12-20 20:57:47 +0530

    progress line

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,29 @@
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import "@/styles/globals.css";
+import { useEffect, useState } from "react";
 
 export default function App({ Component, pageProps }) {
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      const docHeight = document.documentElement.scrollHeight - window.innerHeight;
+      const scrollPercent = (scrollTop / docHeight) * 100;
+      setScrollPosition(scrollPercent);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <>
       <Navbar />
+      <div className="fixed top-0 left-0 w-full h-1 bg-black z-50" style={{ width: `${scrollPosition}%` }} />
       <Component {...pageProps} />
       <Footer />
     </>
-  )
+  );
 }
