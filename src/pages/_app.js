544c86b4c0a6c5e85360704ee3fc86b8520afa2b
Pavan Kumar <pavankumarts2001@gmail.com>
2024-12-26 19:45:58 +0530

    dark mode

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -35,7 +35,7 @@ export default function App({ Component, pageProps }) {
   };
 
   return (
-    <div className="min-h-screen flex flex-col bg-white dark:bg-gray-900 transition-colors duration-200">
+    <div className="min-h-screen flex flex-col transition-colors duration-200">
       <Navbar 
         scrollPosition={scrollPosition} 
         isDarkMode={isDarkMode} 
