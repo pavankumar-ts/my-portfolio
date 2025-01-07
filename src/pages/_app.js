a982f4fb20d1cc3a19850b84ab5c1ddb72aeaec6
Pavan Kumar <pavankumarts2001@gmail.com>
2025-01-08 01:16:16 +0530

    FloatingButton get quote

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -44,7 +44,7 @@ export default function App({ Component, pageProps }) {
         onThemeToggle={handleThemeToggle}
       />
       <main className="flex-grow">
-        {/* <FloatingButton /> */}
+        <FloatingButton />
         <PopupForm />
         <Component {...pageProps} />
       </main>
